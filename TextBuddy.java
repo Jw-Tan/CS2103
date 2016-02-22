6c566c0fd03a4e5ca75220bf85442eb8ebb436e8
Tan Jie Wei <jwtan@hotmail.sg>
2016-02-22 15:13:03 +0800

    Post CE1 Changes
    Edited code according to CE1 feedback

--- TextBuddy.java
+++ TextBuddy.java
@@ -41,8 +41,8 @@ public class TextBuddy {
 	private static final String MESSAGE_NO_TEXT = "Please include desired text in same line with \"add\"";
 	private static final String MESSAGE_LINE_DELETED = "deleted from %1$s: \"%2$s\"";
 	private static final String MESSAGE_NO_LINE_NUMBER = "Please include desired line number in same line with \"delete\"";
-	private static final String MESSAGE_INVALID_LINE_NUMBER = "Invalid line number entered. Please try again.";
-	private static final String MESSAGE_INVALID_COMMAND = "Invalid command. Please try again.";	
+	private static final String MESSAGE_INVALID_LINE_NUMBER = "Invalid line number entered. Please try again";
+	private static final String MESSAGE_INVALID_COMMAND = "Invalid command. Please try again";	
 
 	/**
 	 * Main method of the program.
@@ -57,7 +57,7 @@ public class TextBuddy {
 
 		File myFile = new File(fileName);
 
-		checkIfFileExists(myFile);
+		checkIfFileExistsAndImportIfExists(myFile);
 
 		printWelcomeMessage();
 
@@ -71,7 +71,7 @@ public class TextBuddy {
 	 * 
 	 * @param f The file to be checked.
 	 */
-	private static void checkIfFileExists(File f) throws IOException, FileNotFoundException {
+	public static void checkIfFileExistsAndImportIfExists(File f) throws IOException, FileNotFoundException {
 		if (!f.exists()) {
 			f.createNewFile();
 		} else {
@@ -82,9 +82,9 @@ public class TextBuddy {
 	/**
 	 * Adds all lines of text of the file into an arraylist.
 	 * 
-	 * @param f       The file from which text will be extracted.
+	 * @param f The file from which text will be extracted.
 	 */
-	private static void importLinesFromFile(File f) throws FileNotFoundException {
+	public static void importLinesFromFile(File f) throws FileNotFoundException {
 		Scanner scanner = new Scanner(f);
 		while (scanner.hasNextLine()) {
 			String line = scanner.nextLine();
@@ -93,33 +93,33 @@ public class TextBuddy {
 		scanner.close();
 	}
 
-	private static void printWelcomeMessage() {
+	public static void printWelcomeMessage() {
 		System.out.println(String.format(MESSAGE_WELCOME, fileName));
 	}	
 
 	/**
 	 * Main loop of the program.
 	 */
-	private static void startWaitingForInput() throws IOException {
+	public static void startWaitingForInput() throws IOException {
 		sc = new Scanner(System.in);
 		while (true) {
 			askForCommand();
 			storeCommandFromInput();
 			checkForExitCommand();
-			doCommand();
+			doCommand(inputCommand);
 			saveFile();
 		}
 	}
 
-	private static void askForCommand() {
+	public static void askForCommand() {
 		System.out.print("command: ");
 	}
 
-	private static void storeCommandFromInput() {
+	public static void storeCommandFromInput() {
 		inputCommand = sc.nextLine();
 	}
 
-	private static void checkForExitCommand() {
+	public static void checkForExitCommand() {
 		if (inputCommand.equals("exit")) {
 			System.exit(0);
 		}
@@ -128,16 +128,18 @@ public class TextBuddy {
 	/**
 	 * Compares the command given and performs the appropriate method.
 	 * Informs user if an invalid command was enter.
+	 * 
+	 * @param command User input command.
 	 */
-	private static void doCommand() {
-		if (inputCommand.startsWith("add ")) {
-			tryToAdd();
-		} else if (inputCommand.equals("display")) {
+	public static void doCommand(String command) {
+		if (command.startsWith("add ")) {
+			tryToAdd(currentStrings, command);
+		} else if (command.equals("display")) {
 			printAllLines();
-		} else if (inputCommand.equals("clear")) {
-			clear();
-		} else if (inputCommand.startsWith("delete ")) {
-			tryToDelete();
+		} else if (command.equals("clear")) {
+			clear(currentStrings);
+		} else if (command.startsWith("delete ")) {
+			tryToDelete(currentStrings, command);
 		} else {
 			System.out.println(MESSAGE_INVALID_COMMAND);
 		}
@@ -146,14 +148,32 @@ public class TextBuddy {
 	/**
 	 * Checks if the add command is accompanied by text to be added.
 	 * If so, add the line to the arraylist. If not, informs the user.
+	 * 
+	 * @param arrayList The arraylist to which the intended line will be added.
+	 * @param command User input command.
 	 */
-	private static void tryToAdd() {
-		if (inputCommand.length() > 4) {
-			String stringToAdd = inputCommand.substring(4);
-			addLine(stringToAdd);
+	public static void tryToAdd(ArrayList<String> arrayList, String command) {
+		if (hasTextToAdd(command)) {	
+			addLine(arrayList, extractTextFromCommand(command));
 		} else {
 			System.out.println(MESSAGE_NO_TEXT);
 		}
+	}
+	
+	/**
+	 * Checks if the user input includes text to add.
+	 * Inputs like "add " are invalid and must be detected.
+	 * 
+	 * @param  command User input command.
+	 * @return Whether command length is more than 4.
+	 */
+	public static boolean hasTextToAdd(String command) {
+		return (command.length() > 4);
+	}
+	
+	public static String extractTextFromCommand(String command) {
+		String stringToAdd = command.substring(4);
+		return stringToAdd;
 	}
 
 	/**
@@ -161,8 +181,8 @@ public class TextBuddy {
 	 * 
 	 * @param line The line to be added.
 	 */
-	private static void addLine(String line) {
-		currentStrings.add(line);
+	public static void addLine(ArrayList<String> arrayList, String line) {
+		arrayList.add(line);
 		System.out.println(String.format(MESSAGE_LINE_ADDED, fileName, line));
 	}
 
@@ -171,13 +191,26 @@ public class TextBuddy {
 	 * If so, extracts the line number and attempts to delete that line.
 	 * If not, informs the user. 
 	 */
-	private static void tryToDelete() {
-		if (inputCommand.length() > 7) {
-			int lineToDelete = Integer.parseInt(inputCommand.substring(inputCommand.length() - 1));
-			deleteLine(lineToDelete);
+	public static void tryToDelete(ArrayList<String> arrayList, String command) {
+		if (isCommandLongEnough(command)) {
+			int lineToDelete = extractLineNumberFromCommand(command);
+			deleteLine(arrayList, lineToDelete);
 		} else {
 			System.out.println(MESSAGE_NO_LINE_NUMBER);
 		}
+	}
+	
+	public static boolean isCommandLongEnough(String command) {
+		return (command.length() > 7);
+	}
+	
+	public static int extractLineNumberFromCommand(String command) {
+		try {
+			int lineToDelete = Integer.parseInt(command.substring(7));
+			return lineToDelete;
+		} catch (NumberFormatException e) {
+			return 0;
+        }
 	}
 
 	/**
@@ -186,22 +219,26 @@ public class TextBuddy {
 	 * 
 	 * @param lineNumber The number corresponding to the line that is to be deleted.
 	 */
-	private static void deleteLine(int lineNumber) {
-		if (lineNumber <= currentStrings.size() && lineNumber > 0) {
-			String deletedString = currentStrings.get(lineNumber - 1);
-			currentStrings.remove(lineNumber - 1);
-			System.out.println(String.format(MESSAGE_LINE_DELETED, fileName, deletedString));
-		} else {
+	public static void deleteLine(ArrayList<String> arrayList, int lineNumber) {
+		if (lineNumber <= 0) {
 			System.out.println(MESSAGE_INVALID_LINE_NUMBER);
+		} else {
+			if (lineNumber <= arrayList.size()) {
+				String deletedString = arrayList.get(lineNumber - 1);
+				arrayList.remove(lineNumber - 1);
+				System.out.println(String.format(MESSAGE_LINE_DELETED, fileName, deletedString));
+			} else {
+				System.out.println(MESSAGE_INVALID_LINE_NUMBER);
+			}
 		}
 	}
 
-	private static void clear() {
-		currentStrings.clear();
+	public static void clear(ArrayList<String> arrayList) {
+		arrayList.clear();
 		System.out.println(String.format(MESSAGE_ALL_CLEARED, fileName));
 	}
 
-	private static void printAllLines() {
+	public static void printAllLines() {
 		if (currentStrings.isEmpty()) {
 			System.out.println(String.format(MESSAGE_EMPTY_FILE, fileName));
 		} else {
@@ -212,7 +249,7 @@ public class TextBuddy {
 	/**
 	 * Prints each stored line preceded by an ascending count number.
 	 */
-	private static void printEachLineWithNumbering() {
+	public static void printEachLineWithNumbering() {
 		int count = 0;
 		for (String s : currentStrings) {
 			count++;
@@ -223,7 +260,7 @@ public class TextBuddy {
 	/**
 	 * Writes all lines from the arraylist, if any, into the file.
 	 */
-	private static void saveFile() throws IOException {
+	public static void saveFile() throws IOException {
 		BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, false));
 		makeNewFile();
 		if (currentStrings.isEmpty()) {
@@ -241,7 +278,7 @@ public class TextBuddy {
 	/**
 	 * Creates a new file with the respective filename.
 	 */
-	private static void makeNewFile() throws IOException {
+	public static void makeNewFile() throws IOException {
 		File f = new File(fileName);
 		f.createNewFile();
 	}
