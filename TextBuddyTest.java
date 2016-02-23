c64fa12b6794f61144d1fd1f40fd5c0d8b82b848
Tan Jie Wei <jwtan@hotmail.sg>
2016-02-23 13:07:43 +0800

    Added space

--- TextBuddyTest.java
+++ TextBuddyTest.java
@@ -112,7 +112,7 @@ public class TextBuddyTest {
 		ArrayList<String> testArrayList = new ArrayList<String>();
 		testArrayList.add("this is a test line");
 		
-		//uses the search function with various command inputs and check for correct feedback
+		// uses the search function with various command inputs and check for correct feedback
 		assertEquals("Please include search keyword", TextBuddy.tryToSearch(testArrayList, "search "));
 		assertEquals("There is no line containing the keyword cat", TextBuddy.tryToSearch(testArrayList, "search cat"));
 		assertEquals("1. this is a test line", TextBuddy.tryToSearch(testArrayList, "search test"));
