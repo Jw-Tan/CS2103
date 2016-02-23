cad1f32c97531255d85b9c5dc6bf1db52b3503a9
Tan Jie Wei <jwtan@hotmail.sg>
2016-02-23 11:56:15 +0800

    Added test for delete method

--- TextBuddyTest.java
+++ TextBuddyTest.java
@@ -12,7 +12,8 @@ public class TextBuddyTest {
 	
 	@Before
 	public void setupFileName() {
-		TextBuddy.setFileName("testfile.txt");
+		String testFileName = "testfile.txt";
+		TextBuddy.setFileName(testFileName);
 	}
 
 	@Test
@@ -31,12 +32,35 @@ public class TextBuddyTest {
 		String inputCommand2 = "add ";
 		
 		
-		// uses the add function and checks for correct feedback message, and that arraylist now has size of 1
+		// uses add function and checks for correct feedback message, and that the line is correctly added
 		assertEquals("added to testfile.txt: \"this is a test line\"", TextBuddy.tryToAdd(testArrayList, inputCommand1));
 		assertEquals(1, testArrayList.size());
+		assertEquals("this is a test line", testArrayList.get(0));
 
 		// uses add function with invalid input command and checks for correct feedback message and that no new line is added
 		assertEquals("Please include desired text in same line with \"add\"", TextBuddy.tryToAdd(testArrayList, inputCommand2));
 		assertNotEquals(2, testArrayList.size());
+	}
+
+	@Test
+	public void testTryToDelete() {
+		// creates a dummy arraylist and adds a line to it
+		ArrayList<String> testArrayList = new ArrayList<String>();
+		testArrayList.add("this is a test line");
+		
+		// uses delete function with invalid input (no number) and checks for correct feedback and that no line is deleted
+		String inputCommand1 = "delete ";
+		assertEquals("Please include desired line number in same line with \"delete\"", TextBuddy.tryToDelete(testArrayList, inputCommand1));
+		assertFalse(testArrayList.isEmpty());
+		
+		// uses delete function with invalid input (non-numeric) and checks for correct feedback and that no line is deleted
+		String inputCommand2 = "delete abc";
+		assertEquals("Invalid line number entered. Please try again", TextBuddy.tryToDelete(testArrayList, inputCommand2));
+		assertFalse(testArrayList.isEmpty());
+		
+		// uses delete function with valid input and checks for correct feedback and that the line has been deleted
+		String inputCommand3 = "delete 1";
+		assertEquals("deleted from testfile.txt: \"this is a test line\"", TextBuddy.tryToDelete(testArrayList, inputCommand3));
+		assertTrue(testArrayList.isEmpty());
 	}
 }
